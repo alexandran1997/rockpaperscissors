8a975490559c7df42c20649afa537bd8110afa83
Glitch (alex-rps-game) <none>
2021-11-17 23:28:40 +0000

    🎋🚩 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-
+$(".play").click(function() {
+  let userInput;
+  let userRPS;
+  
+  let computerInput;
+  let computerRPS;
+  
+  let WinState;
+  
+  userInput = $(".input").val();
+  userRPS = "<p>" + userInput + "</p>";
+  $(".userChoice").append(userRPS);
+  
+}
