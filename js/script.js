91f2027a5b137adad444c911fdc87d1f0295908e
Glitch (alex-rps-game) <none>
2022-01-12 23:26:11 +0000

    🚅✍️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,48 +1,43 @@
-//$(".play").click(function() {
-  let userInput;
-  let userRPS;
-  
-  
-  userInput = $(".input").val();
-  userRPS = "<p>" + userInput + "</p>";
-  $(".userChoice").append(userRPS);
-  
 
-                 
- //DAY 2
-                 
+  
+  //DAY 5 (LAST DAY) - Use parameters and arguments
 $(".play").click(function() {
-  let Choice = $(".input").val();
-  $(".userChoice").text(Choice);
   
-});               
-
-$(".play").click(function() {
-  let computerInput = Math.random();
-  computerInput = $(".computerChoice").text(`${computerInput}`);
-});
-
-//DAY 3 - Make computer choice to a string
-
-$(".play").click(function() {
-  let computerReply = Math.random();
-  let computerReplyString;
-
+  let computerReply = Math.random() * 3;
+  let userRPS = $(".input").val();
   
-  if (computerReply <= 1) {
-    let computerReplyString = "paper";
-    
+  $("userChoice").text(userRPS);
+  $(".result"). text("");
+  
+  let computerChoice = "No Choice";
+  if (computerReply >= 1 && computerReply <= 2) {
+    computerChoice = "Paper";
+  }
+  else if (computerReply < 1) {
+    computerChoice = "Scissors";
+  }
+  else 
+  computerChoice = "Rock"
+}
+
+$(".computerChoice").text(computerChoice);
+  
+  let result = "";
+  if (userRPS === computerChoice) {
+    result = "Tie!";
+  } else if {
+    (userRPS === "Paper" && computerChoice === "Scissors") 
+    result = "You lose.";
+  } else if {
+    (userRPS === "Rock" && computerChoice === "Paper") 
+    result = "You lose.";
+  } else if {
+    (userRPS === "Scissors" && computerChoice === "Rock") 
+    result = "You lose.";
+  } else {
+    result = "You win!!";
   }
   
-  else if (computerReply === 1) {
-   let computerReplyString = "rock";
-  }
-  
-    else {
-      let computerReplyString = "scissors";
-    }
-   
-  
+  $(".result").text(result);
   
 });
-
